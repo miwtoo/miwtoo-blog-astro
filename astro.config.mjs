2aff3e415174e63a94d7b34a07c903de4ff034dc
miwtoo <miwyugioh@gmail.com>
2024-03-16 01:02:18 +0700

    feat: :sparkles: add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import react from "@astrojs/react";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://miwtoo.me/',
-  integrations: [mdx(), sitemap(), react()]
+  integrations: [mdx(), sitemap(), react(), partytown({
+    config: {
+      forward: ['dataLayer.push'],
+    },
+  })]
 });
