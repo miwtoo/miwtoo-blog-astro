cebb6760eec2c9df52a356d0af94c0b1f16e1817
miwtoo <miwyugioh@gmail.com>
2024-03-17 14:29:01 +0700

    feat: :sparkles: add robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
-
 import partytown from "@astrojs/partytown";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://miwtoo.me/',
-  integrations: [mdx(), sitemap(), react(), partytown({
+  integrations: [mdx(), sitemap(), react(), robotsTxt(), partytown({
     config: {
-      forward: ['dataLayer.push'],
-    },
+      forward: ['dataLayer.push']
+    }
   })]
 });
